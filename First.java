e030b34a380d2fa5e377b60013cd76ccdca5ae30
kumar <root@kube.master.com>
2019-04-25 19:34:36 +0530

    comment 3 added in First.java

--- First.java
+++ First.java
@@ -1,5 +1,6 @@
   //comment1  
 //comment2
+//comment3
       class First {
       public static void main(String[] arguments) {
         System.out.println("Let's do something using Java technology.");
